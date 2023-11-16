6bdf46b69de7be3aaae1d19a8b88d8e10da05f80
Hassan El Siblani <hallo@siblanco.dev>
2023-11-16 08:09:24 +0100

    chore: testing noice nvim

--- lua/sytec/lazynvim.lua
+++ lua/sytec/lazynvim.lua
@@ -81,6 +81,23 @@ local plugins = {
    "phaazon/hop.nvim",
    "prisma/vim-prisma",
    -- "zbirenbaum/copilot.lua"
+
+    -- lazy.nvim
+    {
+      "folke/noice.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- add any options here
+      },
+      dependencies = {
+          -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+          "MunifTanjim/nui.nvim",
+          -- OPTIONAL:
+          --   `nvim-notify` is only needed, if you want to use the notification view.
+          --   If not available, we use `mini` as the fallback
+          -- "rcarriga/nvim-notify",
+        }
+    }
 }
 
 local opts = {}
