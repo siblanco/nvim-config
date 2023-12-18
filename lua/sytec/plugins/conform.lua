9c9691d56b7fc0b14a2a53f38c898103adef73ec
Hassan El Siblani <hallo@siblanco.dev>
2023-12-18 23:41:52 +0100

    chore: prettier seems not to be able to format these filetypes

--- lua/sytec/plugins/conform.lua
+++ lua/sytec/plugins/conform.lua
@@ -1,7 +1,7 @@
 require("conform").setup({
 	format_on_save = {
 		timeout_ms = 500,
-		lsp_fallback = false,
+		lsp_fallback = true,
 	},
 
 	formatters_by_ft = {
@@ -12,13 +12,10 @@ require("conform").setup({
 		typescript = { "prettierd" },
 		javascriptreact = { "prettierd" },
 		typescriptreact = { "prettierd" },
-		pug = { "prettierd" },
-		handlebars = { "prettierd" },
 		yaml = { "prettierd" },
 		markdown = { "prettierd" },
 		vue = { "prettierd" },
 		json = { "prettierd" },
-		html = { "prettierd" },
 		scss = { "prettierd" },
 		css = { "prettierd" },
 	},
