e32bd49f99b9ec317a0b666668e11fe7dc51af5a
Hassan El Siblani <hallo@siblanco.dev>
2024-01-21 14:13:17 +0100

    chore: version lazy-lock

--- init.lua
+++ init.lua
@@ -4,15 +4,20 @@ require("sytec.autocommands")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("sytec.plugins")
+require("lazy").setup("sytec.plugins", {
+  change_detection = {
+    enabled = true, -- automatically check for config file changes and reload the ui
+    notify = false, -- turn off notifications whenever plugin changes are made
+  },
+})
