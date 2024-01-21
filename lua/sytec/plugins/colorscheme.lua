ab8ee66ce2e38ec03c2628a9457052db5536e925
Hassan El Siblani <hallo@siblanco.dev>
2024-01-21 14:41:09 +0100

    chore: customize colorscheme

--- lua/sytec/plugins/colorscheme.lua
+++ lua/sytec/plugins/colorscheme.lua
@@ -11,100 +11,40 @@ return {
       -- light = "lotus",
       dark = "wave", -- "wave, dragon"
     },
+    -- Remove the background of LineNr, {Sign,Fold}Column and friends
+    colors = {
+      theme = {
+        all = {
+          ui = {
+            bg_gutter = "none"
+          }
+        }
+      }
+    },
+    -- Transparent Floating Windows
+    overrides = function(colors)
+      local theme = colors.theme
+      return {
+        TelescopeTitle = { fg = theme.ui.special, bold = true },
+        TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+        TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+        TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+        TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+        TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+        TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+
+        Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+        PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+        PmenuSbar = { bg = theme.ui.bg_m1 },
+        PmenuThumb = { bg = theme.ui.bg_p2 },
+      }
+    end,
   },
   config = function(_, opts)
     require('kanagawa').setup(opts) -- Replace this with your favorite colorscheme
     vim.cmd("colorscheme kanagawa") -- Replace this with your favorite colorscheme
   end
 }
-
--- Kanagawa Theme (Custom Palette)
--- return {
---   -- https://github.com/rebelot/kanagawa.nvim
---   'rebelot/kanagawa.nvim', -- You can replace this with your favorite colorscheme
---   lazy = false,            -- We want the colorscheme to load immediately when starting Neovim
---   priority = 1000,         -- Load the colorscheme before other non-lazy-loaded plugins
---   opts = {
---     -- Replace this with your scheme-specific settings or remove to use the defaults
---     -- transparent = true,
---     background = {
---       -- light = "lotus",
---       dark = "wave", -- "wave, dragon"
---     },
---     colors = {
---       palette = {
---         -- Background colors
---         sumiInk0 = "#161616", -- modified
---         sumiInk1 = "#181818", -- modified
---         sumiInk2 = "#1a1a1a", -- modified
---         sumiInk3 = "#1F1F1F", -- modified
---         sumiInk4 = "#2A2A2A", -- modified
---         sumiInk5 = "#363636", -- modified
---         sumiInk6 = "#545454", -- modified
---
---         -- Popup and Floats
---         waveBlue1 = "#322C47", -- modified
---         waveBlue2 = "#4c4464", -- modified
---
---         -- Diff and Git
---         winterGreen = "#2B3328",
---         winterYellow = "#49443C",
---         winterRed = "#43242B",
---         winterBlue = "#252535",
---         autumnGreen = "#76A56A", -- modified
---         autumnRed = "#C34043",
---         autumnYellow = "#DCA561",
---
---         -- Diag
---         samuraiRed = "#E82424",
---         roninYellow = "#FF9E3B",
---         waveAqua1 = "#7E9CD8",  -- modified
---         dragonBlue = "#7FB4CA", -- modified
---
---         -- Foreground and Comments
---         oldWhite = "#C8C093",
---         fujiWhite = "#F9E7C0",   -- modified
---         fujiGray = "#727169",
---         oniViolet = "#BFA3E6",   -- modified
---         oniViolet2 = "#BCACDB",  -- modified
---         crystalBlue = "#8CABFF", -- modified
---         springViolet1 = "#938AA9",
---         springViolet2 = "#9CABCA",
---         springBlue = "#7FC4EF", -- modified
---         waveAqua2 = "#77BBDD",  -- modified
---
---         springGreen = "#98BB6C",
---         boatYellow1 = "#938056",
---         boatYellow2 = "#C0A36E",
---         carpYellow = "#FFEE99", -- modified
---
---         sakuraPink = "#D27E99",
---         waveRed = "#E46876",
---         peachRed = "#FF5D62",
---         surimiOrange = "#FFAA44", -- modified
---         katanaGray = "#717C7C",
---       },
---     },
---   },
---   config = function(_, opts)
---     require('kanagawa').setup(opts) -- Replace this with your favorite colorscheme
---     vim.cmd("colorscheme kanagawa") -- Replace this with your favorite colorscheme
---
---     -- Custom diff colors
---     vim.cmd([[
---       autocmd VimEnter * hi DiffAdd guifg=#00FF00 guibg=#005500
---       autocmd VimEnter * hi DiffDelete guifg=#FF0000 guibg=#550000
---       autocmd VimEnter * hi DiffChange guifg=#CCCCCC guibg=#555555
---       autocmd VimEnter * hi DiffText guifg=#00FF00 guibg=#005500
---     ]])
---
---     -- Custom border colors
---     vim.cmd([[
---       autocmd ColorScheme * hi NormalFloat guifg=#F9E7C0 guibg=#1F1F1F
---       autocmd ColorScheme * hi FloatBorder guifg=#F9E7C0 guibg=#1F1F1F
---     ]])
---   end
--- }
 
 -- Sonokai Theme
 -- return {
