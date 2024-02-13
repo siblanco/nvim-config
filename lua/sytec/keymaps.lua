896e939299830985be1632e889ef0387ac9c332b
Hassan El Siblani <hallo@siblanco.dev>
2024-02-13 08:50:33 +0100

    chore: yes, override it

--- lua/sytec/keymaps.lua
+++ lua/sytec/keymaps.lua
@@ -46,5 +46,5 @@ keymap("v", ">", ">gv")
 keymap("v", "p", '"_dP')
 
 -- sessions
-keymap('n', '<leader>ss', ':mks ~/.local/share/nvim/sessions/')
+keymap('n', '<leader>ss', ':mks! ~/.local/share/nvim/sessions/')
 keymap('n', '<leader>sl', ':source ~/.local/share/nvim/sessions/')
