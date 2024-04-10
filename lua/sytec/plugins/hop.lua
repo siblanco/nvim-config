b9b66d71f3929496f61f2997c728eb3bbaa6e32c
Hassan El Siblani <hallo@siblanco.dev>
2024-04-10 08:29:50 +0200

    chore: update hop

--- lua/sytec/plugins/hop.lua
+++ lua/sytec/plugins/hop.lua
@@ -1,32 +1,15 @@
 return {
-  "hadronized/hop.nvim",
+  'smoka7/hop.nvim',
+  version = "*",
   config = function()
     require('hop').setup {
-      create_hl_autocmd = false,
-      keys = 'neitsrlpufd'
+      -- create_hl_autocmd = false,
+      keys = 'tnseridpufcy'
     }
 
     local keymap = vim.keymap
 
-    keymap.set('n', 'h', function()
-      require 'hop'.hint_words()
-    end)
-
-    keymap.set('n', 'l', function()
-      require 'hop'.hint_lines_skip_whitespace()
-    end)
-    keymap.set('v', 'h', function()
-      require 'hop'.hint_words()
-    end)
-
-    keymap.set('v', 'l', function()
-      require 'hop'.hint_lines_skip_whitespace()
-    end)
-
-    -- reimplement f / F
-    -- keymap.set('n', 'f',
-    --   "<cmd>:lua require'hop'.hint_char1({direction = require'hop.hint'.HintDirection.AFTER_CURSOR,current_line_only = true})<cr>")
-    -- keymap.set('n', 'F',
-    --   "<cmd>:lua require'hop'.hint_char1({direction = require'hop.hint'.HintDirection.BEFORE_CURSOR,current_line_only = true})<cr>")
+    keymap.set({ 'n', 'v' }, 'h', "<cmd>:HopWord<cr>")
+    keymap.set({ 'n', 'v' }, 'l', "<cmd>:HopLine<cr>")
   end
 }
