6b11f6353d145d76a6b02eaf84d7701a56c11c69
Hassan El Siblani <hallo@siblanco.dev>
2024-07-17 10:19:46 +0200

    chore: auto fix eslint issuse on save

--- lua/sytec/plugins/conform.lua
+++ lua/sytec/plugins/conform.lua
@@ -3,26 +3,30 @@ return {
   event = { "BufWritePre", "BufNewFile" },
   config = function()
     require("conform").setup({
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-        async = false,
-      },
+      format_on_save = function()
+        -- auto apply all code actions (for eslint autofixes)
+        vim.lsp.buf.code_action({
+          filter = function(a) return a.isPreferred end,
+          apply = true
+        })
+        return { timeout_ms = 500, lsp_fallback = true, async = false }
+      end,
 
       formatters_by_ft = {
         php = { "php_cs_fixer" },
         lua = { "stylua" },
         python = { "isort", "black" },
-        javascript = { "prettierd" },
-        typescript = { "prettierd" },
-        javascriptreact = { "prettierd" },
-        typescriptreact = { "prettierd" },
-        yaml = { "prettierd" },
-        markdown = { "prettierd" },
-        vue = { "prettierd" },
-        json = { "prettierd" },
-        scss = { "prettierd" },
-        css = { "prettierd" },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+        vue = { "prettier" },
+        json = { "prettier" },
+        scss = { "prettier" },
+        css = { "prettier" },
+        mjs = { "prettier" },
         astro = { "prettier" },
         twig = { "twig-cs-fixer" },
         java = { "google-java-format" },
