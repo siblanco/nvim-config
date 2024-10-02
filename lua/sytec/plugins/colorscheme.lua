d6c5a91d374bc540ef1d7b2c66b4d146e094c3b6
Hassan El Siblani <hallo@siblanco.dev>
2024-10-02 11:20:30 +0200

    chore: use kanagawa

--- lua/sytec/plugins/colorscheme.lua
+++ lua/sytec/plugins/colorscheme.lua
@@ -1,36 +1,56 @@
-return {
-  "folke/tokyonight.nvim",
-  lazy     = false,
-  priority = 1000,
-  opts     = {},
-  config   = function()
-    require("tokyonight").setup({
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      style = "storm",        -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-      light_style = "day",    -- The theme is used when the background is set to light
-      transparent = false,    -- Enable this to disable setting the background color
-      terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
-      styles = {
-        -- Style to be applied to different syntax groups
-        -- Value is any valid attr-list value for `:help nvim_set_hl`
-        comments = { italic = true },
-        keywords = { italic = true },
-        functions = {},
-        variables = {},
-        -- Background styles. Can be "dark", "transparent" or "normal"
-        sidebars = "dark",              -- style for sidebars, see below
-        floats = "dark",                -- style for floating windows
-      },
-      sidebars = { "qf", "help" },      -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-      day_brightness = 0.3,             -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-      hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-      dim_inactive = false,             -- dims inactive windows
-      lualine_bold = false,             -- When `true`, section headers in the lualine theme will be bold
-      on_colors = function(colors) end,
-      on_highlights = function(highlights, colors) end,
-    })
+-- return {
+--   "folke/tokyonight.nvim",
+--   lazy     = false,
+--   priority = 1000,
+--   opts     = {},
+--   config   = function()
+--     require("tokyonight").setup({
+--       -- your configuration comes here
+--       -- or leave it empty to use the default settings
+--       style = "storm",        -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+--       light_style = "day",    -- The theme is used when the background is set to light
+--       transparent = false,    -- Enable this to disable setting the background color
+--       terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+--       styles = {
+--         -- Style to be applied to different syntax groups
+--         -- Value is any valid attr-list value for `:help nvim_set_hl`
+--         comments = { italic = true },
+--         keywords = { italic = true },
+--         functions = {},
+--         variables = {},
+--         -- Background styles. Can be "dark", "transparent" or "normal"
+--         sidebars = "dark",              -- style for sidebars, see below
+--         floats = "dark",                -- style for floating windows
+--       },
+--       sidebars = { "qf", "help" },      -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+--       day_brightness = 0.3,             -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+--       hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+--       dim_inactive = false,             -- dims inactive windows
+--       lualine_bold = false,             -- When `true`, section headers in the lualine theme will be bold
+--       on_colors = function(colors) end,
+--       on_highlights = function(highlights, colors) end,
+--     })
+--
+--     vim.cmd [[colorscheme tokyonight-night]]
+--   end
+-- }
 
-    vim.cmd [[colorscheme tokyonight-night]]
+-- kanagawa
+return {
+  -- https://github.com/rebelot/kanagawa.nvim
+  'rebelot/kanagawa.nvim', -- You can replace this with your favorite colorscheme
+  lazy = false,            -- We want the colorscheme to load immediately when starting Neovim
+  priority = 1000,         -- Load the colorscheme before other non-lazy-loaded plugins
+  opts = {
+    -- Replace this with your scheme-specific settings or remove to use the defaults
+    -- transparent = true,
+    background = {
+      -- light = "lotus",
+      dark = "wave", -- "wave, dragon"
+    },
+  },
+  config = function(_, opts)
+    require('kanagawa').setup(opts)        -- Replace this with your favorite colorscheme
+    vim.cmd("colorscheme kanagawa-dragon") -- Replace this with your favorite colorscheme
   end
 }
