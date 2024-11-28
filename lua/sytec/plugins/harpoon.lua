e54536cdbf704c23232f11a8e0dd444fa52eb2e2
Hassan El Siblani <hallo@siblanco.dev>
2024-11-28 11:10:05 +0100

    chore: add back treesitter context, change harpoon bindings

--- lua/sytec/plugins/harpoon.lua
+++ lua/sytec/plugins/harpoon.lua
@@ -1,67 +1,67 @@
 return {
-	"ThePrimeagen/harpoon",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-	},
-	config = function()
-		require("harpoon").setup({
-			global_settings = {
-				-- sets the marks upon calling `toggle` on the ui, instead of require `:w`.
-				save_on_toggle = false,
+  "ThePrimeagen/harpoon",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    require("harpoon").setup({
+      global_settings = {
+        -- sets the marks upon calling `toggle` on the ui, instead of require `:w`.
+        save_on_toggle = false,
 
-				-- saves the harpoon file upon every change. disabling is unrecommended.
-				save_on_change = true,
+        -- saves the harpoon file upon every change. disabling is unrecommended.
+        save_on_change = true,
 
-				-- sets harpoon to run the command immediately as it's passed to the terminal when calling `sendCommand`.
-				enter_on_sendcmd = false,
+        -- sets harpoon to run the command immediately as it's passed to the terminal when calling `sendCommand`.
+        enter_on_sendcmd = false,
 
-				-- closes any tmux windows harpoon that harpoon creates when you close Neovim.
-				tmux_autoclose_windows = false,
+        -- closes any tmux windows harpoon that harpoon creates when you close Neovim.
+        tmux_autoclose_windows = false,
 
-				-- filetypes that you want to prevent from adding to the harpoon list menu.
-				excluded_filetypes = { "harpoon" },
+        -- filetypes that you want to prevent from adding to the harpoon list menu.
+        excluded_filetypes = { "harpoon" },
 
-				-- set marks specific to each git branch inside git repository
-				mark_branch = false,
+        -- set marks specific to each git branch inside git repository
+        mark_branch = false,
 
-				-- enable tabline with harpoon marks
-				tabline = false,
-			},
-			menu = {
-				width = 95,
-			},
-		})
+        -- enable tabline with harpoon marks
+        tabline = false,
+      },
+      menu = {
+        width = 95,
+      },
+    })
 
-		-- Harpoon
-		vim.keymap.set("n", "<leader>e", function()
-			require("harpoon.ui").toggle_quick_menu()
-		end)
-		vim.keymap.set("n", "ti", function()
-			require("harpoon.ui").nav_next()
-		end)
-		vim.keymap.set("n", "tm", function()
-			require("harpoon.ui").nav_prev()
-		end)
-		vim.keymap.set("n", "<leader>a", function()
-			require("harpoon.mark").add_file()
-		end)
-		vim.keymap.set("n", "<leader>x", function()
-			require("harpoon.ui").nav_file(1)
-		end)
-		vim.keymap.set("n", "<leader>c", function()
-			require("harpoon.ui").nav_file(2)
-		end)
-		vim.keymap.set("n", "<leader>d", function()
-			require("harpoon.ui").nav_file(3)
-		end)
-		vim.keymap.set("n", "<leader>r", function()
-			require("harpoon.ui").nav_file(4)
-		end)
-		vim.keymap.set("n", "<leader>s", function()
-			require("harpoon.ui").nav_file(5)
-		end)
-		vim.keymap.set("n", "<leader>t", function()
-			require("harpoon.ui").nav_file(6)
-		end)
-	end,
+    -- Harpoon
+    vim.keymap.set("n", "<leader>e", function()
+      require("harpoon.ui").toggle_quick_menu()
+    end)
+    vim.keymap.set("n", "tl", function()
+      require("harpoon.ui").nav_next()
+    end)
+    vim.keymap.set("n", "th", function()
+      require("harpoon.ui").nav_prev()
+    end)
+    vim.keymap.set("n", "<leader>a", function()
+      require("harpoon.mark").add_file()
+    end)
+    vim.keymap.set("n", "<leader>1", function()
+      require("harpoon.ui").nav_file(1)
+    end)
+    vim.keymap.set("n", "<leader>2", function()
+      require("harpoon.ui").nav_file(2)
+    end)
+    vim.keymap.set("n", "<leader>3", function()
+      require("harpoon.ui").nav_file(3)
+    end)
+    vim.keymap.set("n", "<leader>4", function()
+      require("harpoon.ui").nav_file(4)
+    end)
+    vim.keymap.set("n", "<leader>5", function()
+      require("harpoon.ui").nav_file(5)
+    end)
+    vim.keymap.set("n", "<leader>6", function()
+      require("harpoon.ui").nav_file(6)
+    end)
+  end,
 }
--- lua/sytec/plugins/treesitter.lua
+++ lua/sytec/plugins/treesitter.lua
@@ -2,6 +2,7 @@
 
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = { "nvim-treesitter/nvim-treesitter-context" },
   config = function()
     require("nvim-treesitter.configs").setup({
       highlight = {
