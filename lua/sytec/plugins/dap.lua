e126b24f74e7123b7724065a8b3f495329b5651f
Hassan El Siblani <hallo@siblanco.dev>
2025-02-07 09:23:04 +0100

    chore: disable dpa

--- lua/sytec/plugins/dap.lua
+++ lua/sytec/plugins/dap.lua
@@ -1,88 +1,89 @@
-return {
-  "mfussenegger/nvim-dap",
-
-  dependencies = {
-    "nvim-neotest/nvim-nio",
-
-    {
-      "rcarriga/nvim-dap-ui",
-      -- stylua: ignore
-      keys = {
-        { "<leader>du", function() require("dapui").toggle({}) end, desc = "Dap UI" },
-        { "<leader>de", function() require("dapui").eval() end,     desc = "Eval",  mode = { "n", "v" } },
-      },
-      opts = {},
-      config = function(_, opts)
-        local dap = require("dap")
-        local dapui = require("dapui")
-        dapui.setup(opts)
-        dap.listeners.after.event_initialized["dapui_config"] = function()
-          dapui.open({})
-        end
-      end,
-    },
-
-    -- virtual text for the debugger
-    {
-      "theHamsta/nvim-dap-virtual-text",
-      opts = {},
-    },
-
-    {
-      'mxsdev/nvim-dap-vscode-js',
-      dependencies = {
-        {
-          "microsoft/vscode-js-debug",
-          build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out"
-        }
-      },
-      config = function()
-        local lazy_dir = vim.fn.stdpath("data") .. "/lazy"
-
-        require("dap-vscode-js").setup({
-          node_path = "node",
-          debugger_path = string.format("%s/vscode-js-debug/", lazy_dir),
-          adapters = { 'pwa-node', 'pwa-chrome', 'node', 'chrome' },
-          -- log_file_path = '/Users/sytec/Downloads/dap.log',
-          -- log_file_level = vim.log.levels.TRACE, -- Changed from 'ERROR' to numeric value (1=ERROR)
-        })
-      end
-    }
-  },
-
-  -- stylua: ignore
-  keys         = {
-    { "<leader>dc", function() require("dap").continue() end },
-    { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
-    { "<leader>db", function() require("dap").toggle_breakpoint() end,                                    desc = "Toggle Breakpoint" },
-    { "<leader>dC", function() require("dap").run_to_cursor() end,                                        desc = "Run to Cursor" },
-    { "<leader>dg", function() require("dap").goto_() end,                                                desc = "Go to line (no execute)" },
-    { "<leader>di", function() require("dap").step_into() end,                                            desc = "Step Into" },
-    { "<leader>dj", function() require("dap").down() end,                                                 desc = "Down" },
-    { "<leader>dk", function() require("dap").up() end,                                                   desc = "Up" },
-    { "<leader>dl", function() require("dap").run_last() end,                                             desc = "Run Last" },
-    { "<leader>do", function() require("dap").step_out() end,                                             desc = "Step Out" },
-    { "<leader>dO", function() require("dap").step_over() end,                                            desc = "Step Over" },
-    { "<leader>dp", function() require("dap").pause() end,                                                desc = "Pause" },
-    { "<leader>dr", function() require("dap").repl.toggle() end,                                          desc = "Toggle REPL" },
-    { "<leader>ds", function() require("dap").session() end,                                              desc = "Session" },
-    { "<leader>dt", function() require("dap").terminate() end,                                            desc = "Terminate" },
-    { "<leader>dw", function() require("dap.ui.widgets").hover() end,                                     desc = "Widgets" },
-  },
-
-  config       = function()
-    local js_based_languages = { "typescript", "javascript", "typescriptreact" }
-
-    vim.fn.sign_define('DapBreakpoint', { text = '🟥', texthl = '', linehl = '', numhl = '' })
-    vim.fn.sign_define('DapStopped', { text = '▶️', texthl = '', linehl = '', numhl = '' })
-
-    require('dap.ext.vscode').load_launchjs(nil,
-      {
-        ['pwa-node'] = js_based_languages,
-        ['node'] = js_based_languages,
-        ['chrome'] = js_based_languages,
-        ['pwa-chrome'] = js_based_languages
-      }
-    )
-  end,
-}
+return {}
+-- return {
+--   "mfussenegger/nvim-dap",
+--
+--   dependencies = {
+--     "nvim-neotest/nvim-nio",
+--
+--     {
+--       "rcarriga/nvim-dap-ui",
+--       -- stylua: ignore
+--       keys = {
+--         { "<leader>du", function() require("dapui").toggle({}) end, desc = "Dap UI" },
+--         { "<leader>de", function() require("dapui").eval() end,     desc = "Eval",  mode = { "n", "v" } },
+--       },
+--       opts = {},
+--       config = function(_, opts)
+--         local dap = require("dap")
+--         local dapui = require("dapui")
+--         dapui.setup(opts)
+--         dap.listeners.after.event_initialized["dapui_config"] = function()
+--           dapui.open({})
+--         end
+--       end,
+--     },
+--
+--     -- virtual text for the debugger
+--     {
+--       "theHamsta/nvim-dap-virtual-text",
+--       opts = {},
+--     },
+--
+--     {
+--       'mxsdev/nvim-dap-vscode-js',
+--       dependencies = {
+--         {
+--           "microsoft/vscode-js-debug",
+--           build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out"
+--         }
+--       },
+--       config = function()
+--         local lazy_dir = vim.fn.stdpath("data") .. "/lazy"
+--
+--         require("dap-vscode-js").setup({
+--           node_path = "node",
+--           debugger_path = string.format("%s/vscode-js-debug/", lazy_dir),
+--           adapters = { 'pwa-node', 'pwa-chrome', 'node', 'chrome' },
+--           -- log_file_path = '/Users/sytec/Downloads/dap.log',
+--           -- log_file_level = vim.log.levels.TRACE, -- Changed from 'ERROR' to numeric value (1=ERROR)
+--         })
+--       end
+--     }
+--   },
+--
+--   -- stylua: ignore
+--   keys         = {
+--     { "<leader>dc", function() require("dap").continue() end },
+--     { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
+--     { "<leader>db", function() require("dap").toggle_breakpoint() end,                                    desc = "Toggle Breakpoint" },
+--     { "<leader>dC", function() require("dap").run_to_cursor() end,                                        desc = "Run to Cursor" },
+--     { "<leader>dg", function() require("dap").goto_() end,                                                desc = "Go to line (no execute)" },
+--     { "<leader>di", function() require("dap").step_into() end,                                            desc = "Step Into" },
+--     { "<leader>dj", function() require("dap").down() end,                                                 desc = "Down" },
+--     { "<leader>dk", function() require("dap").up() end,                                                   desc = "Up" },
+--     { "<leader>dl", function() require("dap").run_last() end,                                             desc = "Run Last" },
+--     { "<leader>do", function() require("dap").step_out() end,                                             desc = "Step Out" },
+--     { "<leader>dO", function() require("dap").step_over() end,                                            desc = "Step Over" },
+--     { "<leader>dp", function() require("dap").pause() end,                                                desc = "Pause" },
+--     { "<leader>dr", function() require("dap").repl.toggle() end,                                          desc = "Toggle REPL" },
+--     { "<leader>ds", function() require("dap").session() end,                                              desc = "Session" },
+--     { "<leader>dt", function() require("dap").terminate() end,                                            desc = "Terminate" },
+--     { "<leader>dw", function() require("dap.ui.widgets").hover() end,                                     desc = "Widgets" },
+--   },
+--
+--   config       = function()
+--     local js_based_languages = { "typescript", "javascript", "typescriptreact" }
+--
+--     vim.fn.sign_define('DapBreakpoint', { text = '🟥', texthl = '', linehl = '', numhl = '' })
+--     vim.fn.sign_define('DapStopped', { text = '▶️', texthl = '', linehl = '', numhl = '' })
+--
+--     require('dap.ext.vscode').load_launchjs(nil,
+--       {
+--         ['pwa-node'] = js_based_languages,
+--         ['node'] = js_based_languages,
+--         ['chrome'] = js_based_languages,
+--         ['pwa-chrome'] = js_based_languages
+--       }
+--     )
+--   end,
+-- }
