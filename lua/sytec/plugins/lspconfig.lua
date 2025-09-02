3a7b1f2f22af4f7d0a6528324e8763e7f2db29f0
Hassan El Siblani <hallo@siblanco.dev>
2025-09-02 17:14:42 +0200

    feat: add previous diagnostic navigation keymap

--- lua/sytec/plugins/lspconfig.lua
+++ lua/sytec/plugins/lspconfig.lua
@@ -77,6 +77,7 @@ return {
 
     -- keymaps
     vim.keymap.set("n", "<C-n>", vim.diagnostic.goto_next)
+    vim.keymap.set("n", "<C-p>", vim.diagnostic.goto_prev)
     -- Use LspAttach autocommand to only map the following keys
     -- after the language server attaches to the current buffer
     vim.api.nvim_create_autocmd("LspAttach", {
